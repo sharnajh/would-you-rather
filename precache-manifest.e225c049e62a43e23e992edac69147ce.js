fbf8393c7290a8b24c15d0ccf7fc0fb7d552205b
Sharna Hossain <sharnajhossain@gmail.com>
2019-12-25 11:32:49 -0500

    Updates

---
+++ precache-manifest.e225c049e62a43e23e992edac69147ce.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6d63fb2723c9a3a48a90e8fdea3842",
+    "url": "/sharnajh/would-you-rather/index.html"
+  },
+  {
+    "revision": "858aa8f39c1f824c2cf2",
+    "url": "/sharnajh/would-you-rather/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "62d671598b79a8125020",
+    "url": "/sharnajh/would-you-rather/static/css/main.eb06a0af.chunk.css"
+  },
+  {
+    "revision": "858aa8f39c1f824c2cf2",
+    "url": "/sharnajh/would-you-rather/static/js/2.29302801.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/sharnajh/would-you-rather/static/js/2.29302801.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62d671598b79a8125020",
+    "url": "/sharnajh/would-you-rather/static/js/main.aa49f87d.chunk.js"
+  },
+  {
+    "revision": "5362197f42621d20c64c",
+    "url": "/sharnajh/would-you-rather/static/js/runtime-main.ab1513d5.js"
+  },
+  {
+    "revision": "d5f51047d8bd6327ec4a74361a7aae7f",
+    "url": "/sharnajh/would-you-rather/static/media/avatar.d5f51047.jpg"
+  },
+  {
+    "revision": "528ddb19a5da6518ab007c47fa5756ae",
+    "url": "/sharnajh/would-you-rather/static/media/john-doe.528ddb19.png"
+  },
+  {
+    "revision": "c67fb091855114825a06182d06cedcf8",
+    "url": "/sharnajh/would-you-rather/static/media/sarah-hedo.c67fb091.jpg"
+  },
+  {
+    "revision": "ab3a62a2f3f1cd68ddcd58bcda8d0b01",
+    "url": "/sharnajh/would-you-rather/static/media/tyler-mcginnis.ab3a62a2.jpg"
+  }
+]);

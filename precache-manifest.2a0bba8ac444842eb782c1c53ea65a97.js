b79e34b1fdffb6c6fac6a9ea8b4105fb25ca8d7a
Sharna Hossain <sharnajhossain@gmail.com>
2020-01-16 23:12:30 -0500

    Updates

---
+++ precache-manifest.2a0bba8ac444842eb782c1c53ea65a97.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc709d53fa9f8b482109b706b331caa7",
+    "url": "/would-you-rather/index.html"
+  },
+  {
+    "revision": "4d836ee94d6d86fd4dc9",
+    "url": "/would-you-rather/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b43c5ba8687c6a878285",
+    "url": "/would-you-rather/static/css/main.229fe686.chunk.css"
+  },
+  {
+    "revision": "4d836ee94d6d86fd4dc9",
+    "url": "/would-you-rather/static/js/2.9f29da1a.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/would-you-rather/static/js/2.9f29da1a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b43c5ba8687c6a878285",
+    "url": "/would-you-rather/static/js/main.ec4e5c72.chunk.js"
+  },
+  {
+    "revision": "1ba301353be9602ba55b",
+    "url": "/would-you-rather/static/js/runtime-main.48c27b57.js"
+  },
+  {
+    "revision": "d5f51047d8bd6327ec4a74361a7aae7f",
+    "url": "/would-you-rather/static/media/avatar.d5f51047.jpg"
+  },
+  {
+    "revision": "528ddb19a5da6518ab007c47fa5756ae",
+    "url": "/would-you-rather/static/media/john-doe.528ddb19.png"
+  },
+  {
+    "revision": "c67fb091855114825a06182d06cedcf8",
+    "url": "/would-you-rather/static/media/sarah-hedo.c67fb091.jpg"
+  },
+  {
+    "revision": "ab3a62a2f3f1cd68ddcd58bcda8d0b01",
+    "url": "/would-you-rather/static/media/tyler-mcginnis.ab3a62a2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.eacc4d41d85b3384ddf0080ca3690dd3.js"
+  "/would-you-rather/precache-manifest.2a0bba8ac444842eb782c1c53ea65a97.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/would-you-rather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
